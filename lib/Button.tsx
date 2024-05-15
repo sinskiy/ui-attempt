d2f84a700e79dbc24b861c547927acc79b0bbe82
sinskiy <sinskiy@proton.me>
2024-05-15 15:16:33 +0300

    add basic button styles

---
+++ lib/Button.tsx
@@ -0,0 +1,18 @@
+import { ButtonHTMLAttributes } from "react";
+import { cn } from "./utils";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export const Button = ({ className, children, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={cn(
+        "px-6 py-3 bg-emerald-300 rounded-full hover:bg-emerald-200 active:scale-95 transition-all",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
